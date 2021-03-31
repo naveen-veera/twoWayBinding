fa024ed21107f84db8b3d848f93f5808d3aa080f
Naveen132895 <veeranaveen.sethuraman@faceacademy.in>
2021-03-31 23:42:07 +0530

    Initial Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'twoWayBinding';
+
+  input_value: number = 0;
+  radio_value: string;
+  result: any;
+
+  getResult(): any {
+    if (this.radio_value == "Centigrade") {
+      this.result = this.input_value * (9 / 5) + 32;
+    } else if (this.radio_value == "Fahrenheit") {
+      this.result = (this.input_value - 32) * (5 / 9);
+    } else {
+      this.result = "Invalid Input";
+    }
+    return this.result;
+  }
 }
